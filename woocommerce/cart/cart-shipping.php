4bc0d65db55a7c26be2c2f4d5e2dd93b67889b76
Suleiman Boutzamat <boutzamat@gmail.com>
2019-06-15 16:06:10 +0200

    Fixed Pakkelabels issue
    * Fixed: cart-shipping.php that returned no shops when using Pakkelabels plugin
    * Updated: Pakkelabels custom CSS to match newer versions
    * Updated: Theme version

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -30,20 +30,18 @@ $calculator_text          = '';
 		<?php if ( $available_methods ) : ?>
 			<ul id="shipping_method" class="woocommerce-shipping-methods list-unstyled">
 				<?php foreach ( $available_methods as $method ) : ?>
+					<li class="custom-control custom-radio mb-2">
                     <?php
                     if (1 < count($available_methods)) {
-                        print '<li class="custom-control custom-radio mb-2">';
                         printf('<input type="radio" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method custom-control-input" %4$s />', $index, esc_attr(sanitize_title($method->id)), esc_attr($method->id), checked($method->id, $chosen_method, false)); // WPCS: XSS ok.
                         printf('<label for="shipping_method_%1$s_%2$s" class="custom-control-label">%3$s</label>', $index, esc_attr(sanitize_title($method->id)), wc_cart_totals_shipping_method_label($method)); // WPCS: XSS ok.
-                        print '</li>';
                     } else {
-                        print '<li>';
                         printf('<input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" />', $index, esc_attr(sanitize_title($method->id)), esc_attr($method->id)); // WPCS: XSS ok.
                         printf('<label for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr(sanitize_title($method->id)), wc_cart_totals_shipping_method_label($method)); // WPCS: XSS ok.
-                        print '</li>';
                     }
                     do_action('woocommerce_after_shipping_rate', $method, $index);
                     ?>
+					</li>
 				<?php endforeach; ?>
 			</ul>
 			<?php if ( is_cart() ) : ?>
